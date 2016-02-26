d39a82f9f785e70742556ec92ea148e1828e6bf4
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-26 09:52:54 -0600

    set up basic server route

---
+++ server.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) => {
+  res.send("This is the default route");
+});
+
+
+
+app.listen(PORT, () =>{
+  console.log(`App listening on port ${PORT}`);
+});
